f3d5b66250e0d6c82300f773b48d72b13feafd6d
hugotata78 <hugotatarinoff@gmail.com>
2022-08-31 09:56:31 -0300

    mejoras en validaciones

--- validators/index.js
+++ validators/index.js
@@ -1,9 +1,11 @@
 const { validateLogin } = require('./login.validator')
 const { validateRegister } = require('./register.validator')
 const { validateCharacter } = require('./character.validator')
+const { validateRole } = require('./role.validator')
 
 module.exports = {
     validateLogin,
     validateRegister,
-    validateCharacter
+    validateCharacter,
+    validateRole
 }
