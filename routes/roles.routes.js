8b3c4e6b8f83ecc2e904d64d07b18b4c670afedc
hugotata78 <hugotatarinoff@gmail.com>
2022-08-31 09:56:06 -0300

    implementacion de validacione en ruta role

--- routes/roles.routes.js
+++ routes/roles.routes.js
@@ -1,11 +1,12 @@
 const { Router } = require('express')
 const { createRole,getAllRoles,getRole,updateRole,deleteRole, addUserRole,removeUserRole } = require('../controllers/role.controller')
 const { Auth,isAdmin } = require('../middlewares')
+const { validateRole} = require('../validators')
 
 const router = Router()
 
 router.use(Auth,isAdmin)
-router.post('/', createRole)
+router.post('/', validateRole, createRole)
 router.get('/',getAllRoles)
 router.get('/:id',getRole)
 router.put('/:id',updateRole)
