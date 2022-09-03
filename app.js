9a36fe63edaaf4bf5aadb4d52406bd7c34142390
hugotata78 <hugotatarinoff@gmail.com>
2022-09-03 18:21:27 -0300

    morgan

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-const morgan = require('morgan')
+//const morgan = require('morgan')
 const indexRoutes = require('./routes')
 
 const app = express()
@@ -10,7 +10,7 @@ app.set('port', process.env.PORT || 4000)
 app.set('views',path.join(__dirname,'views'))
 app.set('view engine', 'ejs')
 
-app.use(morgan('dev'))
+//app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.get('/', (req,res)=>{
