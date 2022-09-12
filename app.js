f0f80c78ad6fa4630653c094cf07d7a8102fb02b
hugotata78 <hugotatarinoff@gmail.com>
2022-09-12 10:57:25 -0300

    cambios configuración app

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ app.set('name','Servidor')
 app.set('port', process.env.PORT || 4000)
 app.set('views',path.join(__dirname,'views'))
 app.set('view engine', 'ejs')
+app.use( (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 //app.use(morgan('dev'))
 app.use(cors())
